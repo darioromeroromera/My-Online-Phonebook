a9b924ec4441d83671b1e2aae5f7da7306762dcb
Darío Romero Romera <darioromeroromera@gmail.com>
2024-05-12 19:02:40 +0200

    Terminada funcionalidad añadir con imagen

--- src/components/AddOrUpdateContact.js
+++ src/components/AddOrUpdateContact.js
@@ -2,16 +2,22 @@ import { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import Header from "./Header";
 import './css/AddOrUpdateContact.css';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const AddOrUpdateContact = ({isEdit}) => {
     const navigate = useNavigate();
 
-    // State for form fields
+    const [loading, setLoading] = useState(false);
+
     const [contactName, setContactName] = useState('');
+
     const [fullName, setFullName] = useState('');
+
     const [phoneNumber, setPhoneNumber] = useState('');
+
     const [contactImage, setContactImage] = useState(null);
+
+    const [contactRender, setContactRender] = useState("");
 
     const [contactNameFlag, setContactNameFlag] = useState(false);
 
@@ -25,7 +31,6 @@ const AddOrUpdateContact = ({isEdit}) => {
 
     const [apiError, setApiError] = useState('');
 
-    // State for form validation errors
     const [errors, setErrors] = useState({
         contactName: '',
         fullName: '',
@@ -52,7 +57,10 @@ const AddOrUpdateContact = ({isEdit}) => {
     }
 
     const checkContactImage = () => {
+        console.log(contactImage);
         if (contactImage != null) {
+            setContactRender(URL.createObjectURL(contactImage));
+            console.log(contactRender);
             setErrors(errors => {
                 return (contactImage.type != 'image/jpeg' && contactImage.type != 'image/png') ? { ...errors, contactImage: 'El campo de imagen debe ser formato jpeg o png' } : { ...errors, contactImage: '' };
             });
@@ -75,15 +83,50 @@ const AddOrUpdateContact = ({isEdit}) => {
         contactImageFlag ? checkContactImage() : setContactImageFlag(true);
     }, [contactImage]);
 
-    const addContact = () => {
-        alert('ADD');
+    const addContact = async () => {
+        if (loading)
+            return;
+        setLoading(true);
+        let imageData = null;
+        if (contactImage) {
+            imageData = await readFileAsBase64(contactImage);
+            console.log(imageData);
+        }
+        const data = await fetch('http://localhost:8080/api/contacts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                token: localStorage.getItem('token')
+            },
+            mode: 'cors',
+            body: JSON.stringify({'contact_name': contactName, 'full_name': fullName, 'telefono': phoneNumber, 'contact_picture': imageData})
+        });
+
+        const json = await data.json();
+
+        console.log(json);
+
+        if (json.result === undefined) {
+            setLoading(false);
+            setApiError('Ha ocurrido un error desconocido. Inténtelo más tarde');
+            setIsErrorVisible(true);
+        } else if (json.result === 'error') {
+            setLoading(false);
+            setApiError(json.details);
+            setIsErrorVisible(true);
+        } else {
+            setTimeout(() => {
+                setLoading(false);
+                navigate('/');
+            }, 1000);
+        }
+        
     }
 
     const updateContact = () => {
         alert('EDIT');
     }
 
-    // Function to handle form submission
     const handleSubmit = (e) => {
         e.preventDefault();
         checkContactName();
@@ -97,35 +140,54 @@ const AddOrUpdateContact = ({isEdit}) => {
             } else {
                 addContact();
             }
+    };
 
+    const {id} = useParams();
 
-        
-            // Perform submit action (POST or PUT)
-            // Example:
-            // fetch('http://localhost:8080/api/contacts', {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //         'Authorization': `Bearer ${localStorage.getItem('token')}`
-            //     },
-            //     body: JSON.stringify({ contactName, fullName, phoneNumber, contactImage })
-            // })
-            // .then(response => {
-            //     if (!response.ok) {
-            //         throw new Error('Error al agregar o actualizar el contacto');
-            //     }
-            //     return response.json();
-            // })
-            // .then(data => {
-            //     // Handle success
-            //     console.log('Contact added/updated successfully:', data);
-            //     navigate('/'); // Redirect to home page
-            // })
-            // .catch(error => {
-            //     // Handle error
-            //     console.error('Error:', error);
-            // });
-        
+    const loadContactData = async () => {
+        const data = await fetch('http://localhost:8080/api/contacts/' + id, {
+            headers: {
+                token: localStorage.getItem('token')
+            }
+        });
+
+        const json = await data.json();
+
+        if (json.result === undefined) {
+            setApiError('Ha ocurrido un error desconocido. Inténtelo más tarde');
+            setIsErrorVisible(true);
+        } else if (json.result === 'error') {
+            setApiError(json.details);
+            setIsErrorVisible(true);
+        } else {
+            setContactName(json.data.contact_name);
+            setFullName(json.data.full_name);
+            setPhoneNumber(json.data.telefono);
+            if (json.data.contact_picture != null)
+                setContactRender(json.data.contact_picture);
+            console.log(json.data);
+        }
+    };
+
+    useEffect(() => {
+        if (isEdit)
+            loadContactData();
+    }, []);
+
+    const readFileAsBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+    
+            reader.onload = () => {
+                resolve(`data:${file.type};base64,${reader.result.split(',')[1]}`);
+            };
+    
+            reader.onerror = () => {
+                reject(reader.error);
+            };
+
+            reader.readAsDataURL(file);
+        });
     };
 
     return (
@@ -135,6 +197,12 @@ const AddOrUpdateContact = ({isEdit}) => {
             </Helmet>
 
             <Header />
+
+
+            {loading && 
+                <div className='AddOrUpdateContact__SpinnerDiv'>
+                    <div className='AddOrUpdateContact__Spinner'></div>    
+                </div>}
 
             <div className="AddOrUpdateContact__FormContainer">
                 <form className="AddOrUpdateContact__Form" onSubmit={handleSubmit}>
@@ -172,6 +240,12 @@ const AddOrUpdateContact = ({isEdit}) => {
                     />
 
                     <p className="AddOrUpdateContact__Error">{errors.contactImage}</p>
+
+                    <img className={contactRender == '' ? 'AddOrUpdateContact__Hidden' : 'AddOrUpdateContact__Form__Render'} src={contactRender}/>
+
+                    <div className={isErrorVisible ? 'AddOrUpdateContact__ApiError' : 'AddOrUpdateContact__Hidden'}>
+                        <p>{apiError}</p>
+                    </div>
 
                     <button className="AddOrUpdateContact__Form__Button" type="submit">{isEdit ? 'Guardar cambios' : 'Guardar'}</button>
 
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './css/ContactCard.css';
+import {useNavigate} from 'react-router-dom';
 
 const ContactCard = React.memo(({name, fullname, phone, details, picture, removeContact, id}) => {
+
+    const navigate = useNavigate();
+
     return (
         <div className="ContactCard">
             <img className="ContactCard__Picture" src={picture === null ? 'empty-profile-logo.png' : picture} alt="Foto de contacto"/>
@@ -9,7 +13,8 @@ const ContactCard = React.memo(({name, fullname, phone, details, picture, remove
             <p className="ContactCard__FullName">{fullname}</p>
             <p className="ContactCard__Phone">{phone}</p>
             <p className="ContactCard__Details">{details}</p>
-            <button className="ContactCard__Button" onClick={() => {removeContact(id)}}>Eliminar</button>
+            <button className="ContactCard__UpdateButton" onClick={() => {navigate('/edit/' + id)}}>Modificar</button>
+            <button className="ContactCard__DeleteButton" onClick={() => {removeContact(id)}}>Eliminar</button>
         </div>
     )
 });
